1c66a5b5fef86864242a5fae344a5246d01022a8
Jackson Hales <me@jacksonhales.com.au>
2018-05-28 11:48:00 +1000

    Async calculation of nthPrime

--- Task1_Console/Task1_Console/Program.cs
+++ Task1_Console/Task1_Console/Program.cs
@@ -10,6 +10,50 @@ namespace Task1_Console
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(DateTime.Now);
+            var task = FindPrimesAsync();
+
+            Console.ReadLine();
+
+        }
+
+        public static async Task FindPrimesAsync()
+        {
+            var a = await FindPrime(250000);
+            Console.WriteLine(DateTime.Now);
+            var b = await FindPrime(400000);
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+
+        public static async Task<long> FindPrime(int nthPrimeToFind)
+        {
+            long a = 2;
+            int count = 0;
+
+            while (count < nthPrimeToFind)
+            {
+                long b = 2;
+                int prime = 1;// to check if found a prime
+
+                while (b * b <= a)
+                {
+                    if (a % b == 0)
+                    {
+                        prime = 0;
+                        break;
+                    }
+                    b++;
+                }
+                if (prime > 0)
+                {
+                    count++;
+                }
+                a++;
+            }
+            return (--a);
         }
     }
 }
