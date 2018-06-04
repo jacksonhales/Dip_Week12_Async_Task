f2faa49629e9236be2a087fba8f605b457062b59
Jackson Hales <me@jacksonhales.com.au>
2018-06-04 10:02:22 +1000

    Created WPF app to run async

--- Task2_WPF/Task2_WPF/MainWindow.xaml.cs
+++ Task2_WPF/Task2_WPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,48 @@ namespace Task2_WPF
         public MainWindow()
         {
             InitializeComponent();
+
+            
+        }
+
+        private async void BtnGetJSON_ClickAsync(object sender, RoutedEventArgs e)
+        {
+
+            string requestPage = "https://jsonplaceholder.typicode.com/posts/1/comments";
+
+            // ... Use HttpClient.
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(requestPage))
+            using (HttpContent content = response.Content)
+            {
+                // ... Read the string.
+                string result = await content.ReadAsStringAsync();
+
+                lblJSONfromHTTP.Content = result;
+            }
+        }
+
+        private void btnGetJSON_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+            string requestPage = "https://jsonplaceholder.typicode.com/posts/1/comments";
+
+            // ... Use HttpClient.
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = client.(requestPage))
+            using (HttpContent content = response.Content)
+            {
+                // ... Read the string.
+                string result = await content.ReadAsStringAsync();
+
+                lblJSONfromHTTP.Content = result;
+            }
+            */
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            lblJSONfromHTTP.Content = "";
         }
     }
 }
